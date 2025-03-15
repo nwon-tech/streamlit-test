f8e55da3b0471eb04cd07474983155d998c3d4d7
Nelson Wong <nelsonwongtech@gmail.com>
2025-03-16 02:14:32 +0800

    homepage draft finalised

--- app.py
+++ app.py
@@ -26,10 +26,6 @@ page_by_image = """
 [data-testid="stSidebarContent"]{
     background: rgba(51, 170, 51, 1);
 }
-
-[data-testid="stElementContainer"]{
-    overflow: hidden;
-}
 </style>
 """
 
@@ -65,7 +61,31 @@ def display_and_fetch_data(location):
     
     # Ensure valid latitude and longitude are provided
     if latitude is None or longitude is None:
-        st.error("Latitude or Longitude is missing from the location data.")
+        st.error("Kindly select the location button to check the air quality.")
+        return
+
+    # Build API URL with the given coordinates
+    api_url = f"https://api.waqi.info/feed/geo:{latitude};{longitude}/?token=261ee3243494eb7a36e0edd4deabfcc92eee9880"
+
+    try:
+        response = requests.get(api_url)
+        response.raise_for_status()
+        data = response.json()
+        return data
+    except requests.exceptions.RequestException as e:
+        st.error(f"Failed to retrieve data: {e}")
+
+def placeholder_display_and_fetch_data(location):
+    """
+    Extracts latitude and longitude from the geolocation data,
+    then fetches air quality data using a GET request.
+    """
+    latitude = location.get("latitude")
+    longitude = location.get("longitude")
+    
+    # Ensure valid latitude and longitude are provided
+    if latitude is None or longitude is None:
+        st.error("Kindly select the location button to check the air quality.")
         return
 
     # Build API URL with the given coordinates
@@ -86,10 +106,10 @@ def display_centered_metric(aqi,city,air_quality):
     """
     # HTML code using inline CSS for centering and styling
     html_code = f"""
-    <div style="display: flex; align-items: center; justify-content: center; height: 150px; overflow: hidden;">
-        <div style="background: transparent; padding: 20px; text-align: center; color: white; font-family: Sans-Serif; font-weight: bold; border-radius: 10px; background: rgba(0, 0, 0, 0.5); ">
+    <div style="display: flex; align-items: center; justify-content: center; height: 200px;">
+        <div style="background: transparent; padding: 20px; text-align: center; color: white; font-family: Sans-Serif; font-weight: bold; border-radius: 10px; background: rgba(0, 0, 0, 0.5);">
             <h3 style="margin: 0;">{city}</h3>
-            <h3 style="color: white;">Current Air Quality</h3>
+            <h4 style="color: white;">Current Air Quality</h4>
             <p style="font-size: 48px; margin: 10px 0;">{aqi}</p>
             <h3 style="color: white;">{air_quality}</h3>
         </div>
@@ -97,22 +117,21 @@ def display_centered_metric(aqi,city,air_quality):
     """
     # Use Streamlit's components.html to render the custom HTML component.
     # The height parameter ensures proper layout in the app.
-    components.html(html_code, height=180)
+    components.html(html_code, height=220)
 
 def current_air_quality(payload):
     """
     Extracts and displays the city name and AQI from the API response.
     It uses the custom HTML component to display the AQI in a centered manner.
     """
-    if payload is None:
-        aqi = payload.get("data", {}).get("aqi")
-        city = payload.get("data", {}).get("city", {}).get("name")
+    # if payload is None:
+    #     aqi = payload.get("data", {}).get("aqi")
+    #     city = payload.get("data", {}).get("city", {}).get("name")
     
     aqi = payload.get("data", {}).get("aqi")
     city = payload.get("data", {}).get("city", {}).get("name")
     
     if aqi is None or city is None:
-        st.error("AQI or City name is missing from the payload data.")
         return
     
     air_quality = aqi_rating(aqi)
@@ -121,14 +140,27 @@ def current_air_quality(payload):
     display_centered_metric(aqi,city,air_quality)
 
 # Main app interface
-st.title("Check the Air Quality Index in your area")
+# st.title("Check the Air Quality Index in your area")
 st.write("Click The Icon Below to Begin!")
 
 # Fetch geolocation and air quality data
 location = streamlit_geolocation()
 data = display_and_fetch_data(location)
-current_air_quality(data)
+
+if data is not None:
+    # Display the current air quality data
+    # if data is not None:
+    #     current_air_quality(data)
+    # else:
+    #     st.error("Failed to retrieve data.")
+    current_air_quality(data)
+else:
+    data = {
+        "latitude": 3.139,
+        "longitude": 101.6841
+    }
+    current_air_quality(data)
 
 # Embed an iframe for additional historical data visualisation
-st.header("Air Quality Historical Data Visualised")
-components.iframe("https://lookerstudio.google.com/embed/u/0/reporting/1e01b8fc-0baa-4219-81bd-258967fc09b0/page/f7gAF", height=500)
+# st.header("Air Quality Historical Data Visualised")
+# components.iframe("https://lookerstudio.google.com/embed/u/0/reporting/1e01b8fc-0baa-4219-81bd-258967fc09b0/page/f7gAF", height=500)
