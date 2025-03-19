96d4b8ad0ba6b54fdc9a4c1847fad738e3a3863f
nwon-tech <nelsonwongtech@gmail.com>
2025-03-19 10:23:55 +0800

    workaround for dark location page

--- pages/location.py
+++ pages/location.py
@@ -11,9 +11,7 @@ page_by_image = """
 <style>
 [data-testid="stAppViewContainer"] {
     background: linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%);
-    opacity: 1;
     background-size: cover;
-    background-image: url("https://images.unsplash.com/photo-1534083708493-62fff9d96ecc?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
 }
 
 [data-testid="stHeader"] {
