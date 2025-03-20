d6ad64f7b60cd788828340d2e45989c40c13dde2
nwon-tech <nelsonwongtech@gmail.com>
2025-03-20 14:09:48 +0800

    made minor front end changes to better UX

--- app.py
+++ app.py
@@ -49,12 +49,6 @@ with col2:
 # Inject the CSS styling into the app
 st.markdown(page_by_image, unsafe_allow_html=True)
 
-# Display a notification at the top of the page to notify user that data is sourced from open source
-st.info(
-    "All data is sourced from The World Air Quality Index Project. "
-    "Please note that while the data is openly sourced, users should still take necessary precautions.",icon="ℹ️"
-)
-
 def aqi_rating(aqi):
     """
     Converts the AQI value to a qualitative rating.
@@ -96,9 +90,43 @@ def display_and_fetch_data(location):
 
 def display_centered_metric(aqi,city,air_quality):
 
-    html_code = f"""
+    if (air_quality == "Good"):
+        html_code = f"""
     <div style="display: flex; align-items: center; justify-content: center; height: 200px;">
-        <div style="background: transparent; padding: 20px; text-align: center; color: white; font-family: Sans-Serif; font-weight: bold; border-radius: 10px; background: rgba(0, 0, 0, 0.5);">
+        <div style="background: transparent; padding: 20px; text-align: center; color: white; font-family: Sans-Serif; font-weight: bold; border-radius: 10px; background: rgba(143,185,53,0.5);">
+            <h3 style="margin: 0;">{city}</h3>
+            <h4 style="color: white;">Current Air Quality</h4>
+            <p style="font-size: 48px; margin: 10px 0;">{aqi}</p>
+            <h3 style="color: white;">{air_quality}</h3>
+        </div>
+    </div>
+    """
+    elif (air_quality == "Moderate"):
+        html_code = f"""
+    <div style="display: flex; align-items: center; justify-content: center; height: 200px;">
+        <div style="background: transparent; padding: 20px; text-align: center; color: white; font-family: Sans-Serif; font-weight: bold; border-radius: 10px; background: rgba(230,226,46,0.5);">
+            <h3 style="margin: 0;">{city}</h3>
+            <h4 style="color: white;">Current Air Quality</h4>
+            <p style="font-size: 48px; margin: 10px 0;">{aqi}</p>
+            <h3 style="color: white;">{air_quality}</h3>
+        </div>
+    </div>
+    """
+    elif (air_quality == "Unhealthy for Sensitive Groups"):
+        html_code = f"""
+    <div style="display: flex; align-items: center; justify-content: center; height: 200px;">
+        <div style="background: transparent; padding: 20px; text-align: center; color: white; font-family: Sans-Serif; font-weight: bold; border-radius: 10px; background: rgba(224,156,59,0.5);">
+            <h3 style="margin: 0;">{city}</h3>
+            <h4 style="color: white;">Current Air Quality</h4>
+            <p style="font-size: 48px; margin: 10px 0;">{aqi}</p>
+            <h3 style="color: white;">{air_quality}</h3>
+        </div>
+    </div>
+    """
+    else:
+        html_code = f"""
+    <div style="display: flex; align-items: center; justify-content: center; height: 200px;">
+        <div style="background: transparent; padding: 20px; text-align: center; color: white; font-family: Sans-Serif; font-weight: bold; border-radius: 10px; background: rgba	(230,71,71,0.5);">
             <h3 style="margin: 0;">{city}</h3>
             <h4 style="color: white;">Current Air Quality</h4>
             <p style="font-size: 48px; margin: 10px 0;">{aqi}</p>
@@ -113,11 +141,11 @@ def aqi_recommendation(air_quality):
     Provides recommendations based on the AQI value.
     """
     recommendations = {
-        "Good": "Encourage children to enjoy outdoor sports. Maintain good living habits and avoid contact with allergens. Keep indoor air circulation to let children breathe fresh air. Pay attention to weather changes and avoid long-term exposure to strong sunlight.",
-        "Moderate": "Pay attention to children’s health. Sensitive children should avoid strenuous exercise. If children need to go out, it is recommend to wear a protective mask. Open windows appropriately for ventilation and keep the air flowing. Use an air purifier to reduce pollutants in the air.",
-        "Unhealthy for Sensitive Groups": "Limit long outdoor activities and avoid strenuous exercise. If children need to go out, try to choose a time with better air quality (early morning or evening), and it is recommended to wear a mask with a higher protection level. Reduce the time of opening windows for ventilation. Monitor the health of your child. If unwell, take a rest as soon as possible and consider seeking medical attention as appropriate. Drinking plenty of water can help keep the respiratory tract moist.",
-        "Unhealthy": "Avoid all outdoor activities, and carry out low-intensity indoor activities appropriately. Close doors and windows, and use air purifiers. If children must go out, it is recommended to wear N95 or KN95 level protective masks. Rehydrate and use saline to clean the nasal cavity. Pay close attention to whether your child has the following symptoms: persistent cough, sore throat; shortness of breath, chest tightness; red and swollen eyes or discomfort; dizziness, fatigue. If the symptoms worsen, please seek medical attention in time, especially for children with asthma, who need to carry an inhaler with them.",
-        "Unknown": "Air quality is considered satisfactory, and air pollution poses little or no risk."
+        "Good": "Recommendation: Encourage children to enjoy outdoor sports. Maintain good living habits and avoid contact with allergens. Keep indoor air circulation to let children breathe fresh air. Pay attention to weather changes and avoid long-term exposure to strong sunlight.",
+        "Moderate": "Recommendation: Pay attention to children’s health. Sensitive children should avoid strenuous exercise. If children need to go out, it is recommend to wear a protective mask. Open windows appropriately for ventilation and keep the air flowing. Use an air purifier to reduce pollutants in the air.",
+        "Unhealthy for Sensitive Groups": "Recommendation: Limit long outdoor activities and avoid strenuous exercise. If children need to go out, try to choose a time with better air quality (early morning or evening), and it is recommended to wear a mask with a higher protection level. Reduce the time of opening windows for ventilation. Monitor the health of your child. If unwell, take a rest as soon as possible and consider seeking medical attention as appropriate. Drinking plenty of water can help keep the respiratory tract moist.",
+        "Unhealthy": "Recommendation: Avoid all outdoor activities, and carry out low-intensity indoor activities appropriately. Close doors and windows, and use air purifiers. If children must go out, it is recommended to wear N95 or KN95 level protective masks. Rehydrate and use saline to clean the nasal cavity. Pay close attention to whether your child has the following symptoms: persistent cough, sore throat; shortness of breath, chest tightness; red and swollen eyes or discomfort; dizziness, fatigue. If the symptoms worsen, please seek medical attention in time, especially for children with asthma, who need to carry an inhaler with them.",
+        "Unknown": "Recommendation: Air quality is considered satisfactory, and air pollution poses little or no risk."
     }
 
     if air_quality == "Good":
@@ -144,6 +172,7 @@ def current_air_quality(payload):
         return
     
     air_quality = aqi_rating(aqi)
+    st.write(air_quality);
     
     # custom function to display centered AQI
     display_centered_metric(aqi,city,air_quality)
@@ -152,7 +181,15 @@ def current_air_quality(payload):
     aqi_recommendation(air_quality)
 
 # Main app interface
-st.title("Check the Air Quality Index in your Area")
+st.title("Pollucheck")
+
+# Display a notification at the top of the page to notify user that data is sourced from open source
+st.info(
+    "All data is sourced from The World Air Quality Index Project. "
+    "Please note that while the data is openly sourced, users should still take necessary precautions.",icon="ℹ️"
+)
+
+st.header("Check the Air Quality Index in your Area")
 st.write("Click The Icon ⌖ Below to Begin!")
 
 # Fetch geolocation and air quality data
@@ -160,11 +197,6 @@ location = streamlit_geolocation()
 data = display_and_fetch_data(location)
 
 if data is not None:
-    # Display the current air quality data
-    # if data is not None:
-    #     current_air_quality(data)
-    # else:
-    #     st.error("Failed to retrieve data.")
     current_air_quality(data)
 else:
     data = {
@@ -172,6 +204,8 @@ else:
         "longitude": 101.6841
     }
     current_air_quality(data)
+
+
 
 # Embed an iframe for additional historical data visualisation
 st.header("Air Quality Historical Data Visualised")
--- pages/location.py
+++ pages/location.py
@@ -43,6 +43,10 @@ page_by_image = """
     color: rgba(0, 0, 0, 0.5);
 }
 
+[data-testid="stAlert"]{
+    opacity: 1;    
+}
+
 </style>
 """
 # Acts as a navigation bar for the app
@@ -193,9 +197,43 @@ def aqi_rating(aqi):
     
 def display_centered_metric(aqi,city,air_quality):
 
-    html_code = f"""
+    if (air_quality == "Good"):
+        html_code = f"""
     <div style="display: flex; align-items: center; justify-content: center; height: 200px;">
-        <div style="background: transparent; padding: 20px; text-align: center; color: white; font-family: Sans-Serif; font-weight: bold; border-radius: 10px; background: rgba(0, 0, 0, 0.5);">
+        <div style="background: transparent; padding: 20px; text-align: center; color: white; font-family: Sans-Serif; font-weight: bold; border-radius: 10px; background: rgba(143,185,53,0.5);">
+            <h3 style="margin: 0;">{city}</h3>
+            <h4 style="color: white;">Current Air Quality</h4>
+            <p style="font-size: 48px; margin: 10px 0;">{aqi}</p>
+            <h3 style="color: white;">{air_quality}</h3>
+        </div>
+    </div>
+    """
+    elif (air_quality == "Moderate"):
+        html_code = f"""
+    <div style="display: flex; align-items: center; justify-content: center; height: 200px;">
+        <div style="background: transparent; padding: 20px; text-align: center; color: white; font-family: Sans-Serif; font-weight: bold; border-radius: 10px; background: rgba(230,226,46,0.5);">
+            <h3 style="margin: 0;">{city}</h3>
+            <h4 style="color: white;">Current Air Quality</h4>
+            <p style="font-size: 48px; margin: 10px 0;">{aqi}</p>
+            <h3 style="color: white;">{air_quality}</h3>
+        </div>
+    </div>
+    """
+    elif (air_quality == "Unhealthy for Sensitive Groups"):
+        html_code = f"""
+    <div style="display: flex; align-items: center; justify-content: center; height: 200px;">
+        <div style="background: transparent; padding: 20px; text-align: center; color: white; font-family: Sans-Serif; font-weight: bold; border-radius: 10px; background: rgba(224,156,59,0.5);">
+            <h3 style="margin: 0;">{city}</h3>
+            <h4 style="color: white;">Current Air Quality</h4>
+            <p style="font-size: 48px; margin: 10px 0;">{aqi}</p>
+            <h3 style="color: white;">{air_quality}</h3>
+        </div>
+    </div>
+    """
+    else:
+        html_code = f"""
+    <div style="display: flex; align-items: center; justify-content: center; height: 200px;">
+        <div style="background: transparent; padding: 20px; text-align: center; color: white; font-family: Sans-Serif; font-weight: bold; border-radius: 10px; background: rgba(230,71,71,0.5);">
             <h3 style="margin: 0;">{city}</h3>
             <h4 style="color: white;">Current Air Quality</h4>
             <p style="font-size: 48px; margin: 10px 0;">{aqi}</p>
@@ -210,11 +248,11 @@ def aqi_recommendation(air_quality):
     Provides recommendations based on the AQI value.
     """
     recommendations = {
-        "Good": "Encourage children to enjoy outdoor sports. Maintain good living habits and avoid contact with allergens. Keep indoor air circulation to let children breathe fresh air. Pay attention to weather changes and avoid long-term exposure to strong sunlight.",
-        "Moderate": "Pay attention to children’s health. Sensitive children should avoid strenuous exercise. If children need to go out, it is recommend to wear a protective mask. Open windows appropriately for ventilation and keep the air flowing. Use an air purifier to reduce pollutants in the air.",
-        "Unhealthy for Sensitive Groups": "Limit long outdoor activities and avoid strenuous exercise. If children need to go out, try to choose a time with better air quality (early morning or evening), and it is recommended to wear a mask with a higher protection level. Reduce the time of opening windows for ventilation. Monitor the health of your child. If unwell, take a rest as soon as possible and consider seeking medical attention as appropriate. Drinking plenty of water can help keep the respiratory tract moist.",
-        "Unhealthy": "Avoid all outdoor activities, and carry out low-intensity indoor activities appropriately. Close doors and windows, and use air purifiers. If children must go out, it is recommended to wear N95 or KN95 level protective masks. Rehydrate and use saline to clean the nasal cavity. Pay close attention to whether your child has the following symptoms: persistent cough, sore throat; shortness of breath, chest tightness; red and swollen eyes or discomfort; dizziness, fatigue. If the symptoms worsen, please seek medical attention in time, especially for children with asthma, who need to carry an inhaler with them.",
-        "Unknown": "Air quality is considered satisfactory, and air pollution poses little or no risk."
+        "Good": "Recommendation: Encourage children to enjoy outdoor sports. Maintain good living habits and avoid contact with allergens. Keep indoor air circulation to let children breathe fresh air. Pay attention to weather changes and avoid long-term exposure to strong sunlight.",
+        "Moderate": "Recommendation: Pay attention to children’s health. Sensitive children should avoid strenuous exercise. If children need to go out, it is recommend to wear a protective mask. Open windows appropriately for ventilation and keep the air flowing. Use an air purifier to reduce pollutants in the air.",
+        "Unhealthy for Sensitive Groups": "Recommendation: Limit long outdoor activities and avoid strenuous exercise. If children need to go out, try to choose a time with better air quality (early morning or evening), and it is recommended to wear a mask with a higher protection level. Reduce the time of opening windows for ventilation. Monitor the health of your child. If unwell, take a rest as soon as possible and consider seeking medical attention as appropriate. Drinking plenty of water can help keep the respiratory tract moist.",
+        "Unhealthy": "Recommendation: Avoid all outdoor activities, and carry out low-intensity indoor activities appropriately. Close doors and windows, and use air purifiers. If children must go out, it is recommended to wear N95 or KN95 level protective masks. Rehydrate and use saline to clean the nasal cavity. Pay close attention to whether your child has the following symptoms: persistent cough, sore throat; shortness of breath, chest tightness; red and swollen eyes or discomfort; dizziness, fatigue. If the symptoms worsen, please seek medical attention in time, especially for children with asthma, who need to carry an inhaler with them.",
+        "Unknown": "Recommendation: Air quality is considered satisfactory, and air pollution poses little or no risk."
     }
 
     if air_quality == "Good":
@@ -228,7 +266,8 @@ def aqi_recommendation(air_quality):
     else:
         st.error(recommendations.get("Unknown"))
 
-st.title("Destination AQI")
+st.title("Pollucheck")
+st.header("Destination AQI")
 st.write(
     """
     Planning a trip? Use this Destination AQI to check for the real-time AQI at your preferred location!
